1f7d2a56c5afcc40d89a2cf5b5ad515c659fd5e4
deruyter92 <deruyter92@gmail.com>
2022-02-28 14:30:34 +0100

    fixed unimportant typo

--- Python/training.py
+++ Python/training.py
@@ -283,7 +283,7 @@ def main(config_file=None, specs_file=None):
             if not all(train_specs.index.isin(existing_specs.index)):
                 new_specs = train_specs[~train_specs.index.isin(existing_specs.index)]
                 train_specs = pd.concat([existing_specs, new_specs])
-                print("adding new models to the specs list: \n{}".format('\n'.join(new.index.tolist())))
+                print("adding new models to the specs list: \n{}".format('\n'.join(new_specs.index.tolist())))
 
     else:
         # Load training specficiations from the provided specs list
